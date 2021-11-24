0160ba841e598346b41b94c35928163b9b042780
SiHyeon Bae <baesh0408@naver.com>
2021-11-25 00:08:24 +0900

    delete RSA

--- CN02_11_201702022_배시현/config/passport.js
+++ CN02_11_201702022_배시현/config/passport.js
@@ -3,8 +3,8 @@ var GoogleStrategy = require('passport-google-oauth2').Strategy;
 
 passport.use(new GoogleStrategy(
         {
-                clientID : '55986542851-s2drdcm0c86p6a2ud8bkn1uv2cv79e2g.apps.googleusercontent.com',
-                clientSecret : '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',
+                clientID : '',
+                clientSecret : '',
 		callbackURL : '/auth/google/callback',
 		passReqToCallback : true
 	}, function(request, accessToken, refreshToken, profile, done) {
